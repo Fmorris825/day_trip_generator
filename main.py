1661b05ec4b36d0651fdfd04684bec0c0a7e4f6d
Fred Morris <FredrickCPMorris@hotmail.com>
2022-09-16 13:25:48 -0500

    List Creation

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#Test Commit
+destinations = ["Egypt", "Amsterdam", "Nairobi","Abu Dabi" ]
+resturants = ["Papadeaeuxs", "Lucas", "Grant Steakhouse", "Grillers"]
+transportations = ["Scooter", "Motorcycle", "Taxi", "Uber"]
+excursions = ["Zoo", "Aquarium", "Mountain Climbing", "Museum"]
