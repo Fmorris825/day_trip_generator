e833bf167f2f0941b64f5d09e2692a67638de817
Fred Morris <FredrickCPMorris@hotmail.com>
2022-09-16 13:31:21 -0500

    Random functions created for list

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import random
+
 destinations = ["Egypt", "Amsterdam", "Nairobi","Abu Dabi" ]
 resturants = ["Papadeaeuxs", "Lucas", "Grant Steakhouse", "Grillers"]
 transportations = ["Scooter", "Motorcycle", "Taxi", "Uber"]
 excursions = ["Zoo", "Aquarium", "Mountain Climbing", "Museum"]
+
+def random_destination():
+    random.choice(destinations)
+
+def random_resturant():
+    random.choice(resturants)
+
+def random_resturant():
+    random.choice(transportations)
+
+def random_resturant():
+    random.choice(excursions)
+
