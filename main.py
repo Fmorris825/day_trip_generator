34f305aca9d779812ad725296db8dbc822656805
Fred Morris <FredrickCPMorris@hotmail.com>
2022-09-18 14:30:32 -0500

    all functions wok properly

--- main.py
+++ main.py
@@ -27,30 +27,34 @@ def random_excursion():
     return Excursion
 
 def day_trip_Question():
-    random_destination()
-    random_resturant()
-    random_transportation()
-    random_excursion()
-    return [random_destination(), random_resturant(), random_transportation(), random_excursion()]
-
-d1 = random_destination()
-r1 = random_resturant()
-t1 =  random_transportation()
-e1 = random_excursion()
-day_trip = [d1, r1, t1, e1]
+    Destination = random_destination()
+    Resturant = random_resturant()
+    Transportation = random_transportation()
+    Excursion = random_excursion()
+    return [Destination, Resturant, Transportation, Excursion]
+    
+Destination = random_destination()
+Resturant = random_resturant()
+Transportation =  random_transportation()
+Excursion = random_excursion()
+day_trip = [Destination, Resturant, Transportation, Excursion]
 
 def confirm_trip():
     confirmation = input("Please type 'Confirm' if the trip looks complete. ")
     if confirmation == "Confirm":
-        print (day_trip + [ 'Completed!'])
+        print([random_destination(),random_resturant(),random_transportation(),random_excursion()] + [ 'Completed!'])
 
 def satisfied_with_trip():
+
     satisfied = input("Do you like the trip we have planned? Yes or No? ")
     if satisfied == "Yes":
-        print(day_trip)
+        print(random_destination(),random_resturant(),random_transportation(),random_excursion())
         confirm_trip()
     else:
-        day_trip_Question()
+        random_destination()
+        random_resturant()
+        random_transportation()
+        random_excursion()
         satisfied_with_trip()
 
 satisfied_with_trip()
