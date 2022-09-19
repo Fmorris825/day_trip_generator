5a4c13454173279a7fd16dbc39b403ac21a7732b
Fred Morris <FredrickCPMorris@hotmail.com>
2022-09-19 11:58:22 -0500

    Edit function names to specify actions, final commit for code review

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
 import random
-from tabnanny import check
-
 
 destinations = ["Egypt", "Amsterdam", "Nairobi","Abu Dabi" ]
 resturants = ["Papadeaeuxs", "Lucas", "Grant Steakhouse", "Grillers"]
 transportations = ["Scooter", "Motorcycle", "Taxi", "Uber"]
 excursions = ["Zoo", "Aquarium", "Mountain Climbing", "Museum"]
 
-def random_destination():
+def get_random_destination():
     Destination = "Destination: " + random.choice(destinations)
     print(Destination)
     return Destination
 
-def random_resturant():
+def get_random_resturant():
     Resturant = "Resturant: " + random.choice(resturants)
     print(Resturant)
     return Resturant
 
-def random_transportation():
+def get_random_transportation():
     Transportation = "Transportation: " + random.choice(transportations)
     print(Transportation)
     return Transportation
 
-def random_excursion():
+def get_random_excursion():
     Excursion = "Excursion: " + random.choice(excursions)
     print(Excursion)
     return Excursion
 
-def day_trip_Question():
-    Destination = random_destination()
-    Resturant = random_resturant()
-    Transportation = random_transportation()
-    Excursion = random_excursion()
+def get_trip_itenerary():
+    Destination = get_random_destination()
+    Resturant = get_random_resturant()
+    Transportation = get_random_transportation()
+    Excursion = get_random_excursion()
     return [Destination, Resturant, Transportation, Excursion]
     
 
-day_trip = day_trip_Question()
+day_trip = get_trip_itenerary()
 
 def confirm_trip():
     confirmation = input("Would you like to Rerun the Day trip generator or Confirm your trip itnerary? Please Enter 'Confirm' or 'Rerun'. ")
     if confirmation == "Confirm":
         print(day_trip + [ 'Completed!'])
     elif confirmation == "Rerun":
-        rerun = day_trip_Question()
+        rerun = get_trip_itenerary()
         satisfied = input("Do you like the trip itenerary we have planned for you? Please enter 'Yes' or 'No'? ")
         if satisfied == "Yes":
             print(rerun)
@@ -53,12 +51,12 @@ def confirm_trip():
         confirm_trip()
 
 
-def satisfied_with_trip():
+def get_satisfaction_confirmatino():
     satisfied = input("Do you like the trip we have planned? Yes or No? ")
     if satisfied == "Yes":
         print(day_trip)
     else:
-        rerun = day_trip_Question()
+        rerun = get_trip_itenerary()
         satisfied = input("Do you like the trip we have planned? Yes or No? ")
         if satisfied == "Yes":
             print(rerun)
@@ -68,5 +66,5 @@ def satisfied_with_trip():
         
         
   
-satisfied_with_trip()
+get_satisfaction_confirmatino()
 confirm_trip()
